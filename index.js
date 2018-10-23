4268705d69a289c0068841a87535bc2d8f68b0ce
Ethan Fox <Ethan.Fox@ibm.com>
2018-10-23 13:33:02 -0400

    jess

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ function postMessage(body){
 }
 
 async function getDisplayName(raw_name){
-    console.log('raw name: ' + raw_name)
+
     var result = await axios({
         method: 'post',
         url: 'https://slack.com/api/users.info?token=xoxb-425527920966-461615227600-HtDz46TBLLwOPRK5z3MutyxD&user=' + raw_name
     });
 
+    console.log(result.data)
     if(result.data.ok){
         return result.data.user.profile.display_name
     }else{
@@ -52,11 +53,9 @@ app.post('/', async function (req, res) {
         }else{
             var args = req.body.text.split(' ');
             var raw_receiver = args[0].split('|')[0];
-            console.log('scrubbed receiver: <' + raw_receiver.substring(2, raw_receiver.length) + '>')
+
             var receiver = await getDisplayName(raw_receiver.substring(2, raw_receiver.length));
             var giver = await getDisplayName(req.body.user_id);
-
-            console.log('raw receiver: ' + raw_receiver)
 
             console.log('Giver: ' + giver)
             console.log('Receiver: ' + receiver)
